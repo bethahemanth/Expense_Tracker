a3c0756434acbcd2774d20110defc9e8e68dbe80
hemanthb <hemanthb@VMLAP319>
2025-01-16 11:11:40 +0530

    Add project files.

---
+++ Expense_Tracker/Expense/MainUsage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Expense_Tracker.Basic
+{
+    public class MainUsage
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the number to do the operation");
+                Console.WriteLine("1:To add an expense");
+                Console.WriteLine("2:To view all expenses");
+                Console.WriteLine("3:To edit an expense");
+                Console.WriteLine("4:To delete an expense");
+                int r = Convert.ToInt32(Console.ReadLine());
+                switch (r)
+                {
+                    case 1:
+                        Expense.AddExpense();
+                        break;
+                    case 2:
+                        Expense.ViewAllExpenses();
+                        break;
+                    default:
+                        Console.WriteLine("This is a default message");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Expense_Tracker/Expense/Program.cs
@@ -0,0 +1,77 @@
+﻿// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+
+
+
+
+using System;
+using System.Collections.Generic;
+// Add an expense (date, category, amount, description).
+// View all recorded expenses.
+// Calculate the total expense.
+// Filter expenses by category or date dynamically.
+namespace model
+{ 
+public class Expense
+{
+       // date, category, amount, description.
+       private DateTime date;
+       private string category;
+       private double amount;
+       private string description;
+       public DateTime _date
+       {
+           get
+           {
+               return date;
+           }
+           set{
+               date=value;
+           }
+       }
+       public Expense(DateTime date,string category,double amount,string description)
+       {
+           this.date=date;
+           this.category=category;
+           this.amount=amount;
+           this.description=description;
+       }
+       public static List<Expense> l=new List<Expense>();
+       public static void AddExpense()
+       {
+        Console.WriteLine("Enter the date (yyyy-MM-dd):");
+        DateTime date = Convert.ToDateTime(Console.ReadLine());
+
+
+
+        Console.WriteLine("Enter the category:");
+        string category = Console.ReadLine();
+
+
+
+        Console.WriteLine("Enter the amount:");
+        double amount = Convert.ToDouble(Console.ReadLine());
+
+
+
+        Console.WriteLine("Enter the description:");
+        string description = Console.ReadLine();
+        
+         Expense AddExpense=new Expense(date,category,amount,description);
+         l.Add(AddExpense);
+       }
+       //To view all expenses
+       public static void ViewAllExpenses()
+       {
+         foreach(Expense i in l)
+         {
+           Console.WriteLine(i);
+         }
+       }
+       public override string ToString()
+       {
+           return $"Date: {date.ToShortDateString()}, Category: {category}, Amount: ${amount:F2}, Description: {description}";
+       }
+        }
+   }    
